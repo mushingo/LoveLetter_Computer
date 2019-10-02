aff5463b96d05dba07ca6be5ec8633ced38951ab
mushingo <46770342+mushingo@users.noreply.github.com>
2019-10-02 12:30:54 +0900

    Header file for functions shared by hub.c and player.c

---
+++ shared.h
@@ -0,0 +1,11 @@
+/* Shared.h - Michael Scotson
+ * */
+
+#ifndef SHARED_H_
+#define  SHARED_H_
+int check_card(char card);
+int check_player(char player, int players);
+int check_valid_move(char source, char discard, char target, char guess,
+        int players);
+
+#endif
